6e32a9ccbb9502445b14f70723e56566ad9a93a9
MFuentes01 <megax.yt.uy@gmail.com>
2023-10-13 11:09:50 -0300

    Avances_VIernes

--- init.js
+++ init.js
@@ -1,3 +1,9 @@
+const cartBtn = document.getElementById("btn");
+//const input = document.getElementsByClassName("inputCant");
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
 
     //Declaración de constantes y variables
@@ -8,6 +14,8 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch(url)
     .then(response => response.json())
     .then(data => {
+
+        
 
         data.forEach(articulo => {
 
@@ -20,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         <p class="info">${articulo.descripcion}</p>
                         <p class="costo">${articulo.tipo_moneda} ${articulo.costo}</p>
                         <input type="number" class="inputCant" min="1" required>
-                        <button class="btn" onclick="agregarAlCarrito('${articulo.nombre}', ${articulo.stock})">Agregar al Carrito</button>
+                        <button class="btn" onclick="agregarAlCarrito('${articulo.nombre}', ${articulo.stock}, ${articulo.tipo_moneda}, ${articulo.costo}, this)">Agregar al Carrito</button>
             </div>
                 </div>
                      </div>
@@ -28,18 +36,73 @@ document.addEventListener('DOMContentLoaded', function () {
 
         });
 
-        function agregarAlCarrito(nombre, stock) {
-            const cantidadInput = document.querySelector(".inputCant");
-            const cantidad = cantidadInput.value;
-            const nombreLadrillo = nombre;
-            const stockLadrillo = stock - cantidad;
-            console.log(nombreLadrillo);
-            console.log(stockLadrillo);
-        }
+
+        
 
 
 
     })
 
+    cartBtn.addEventListener("click", function() {
+
+
+
+
+        window.location.href = "cart.html";
+
+    })
+
 
 });
+
+function limpiar() {
+    cantidad = "0";
+    nombreLadrillo = "";
+    stockLadrillo = "";
+    monedaLadrillo = "";
+    costoLadrillo = "";
+    nuevoStockLadrillo = "";
+};
+
+let stockLadrillo;
+let nuevoStockLadrillo;
+
+
+
+const stocks = {}; // Objeto para almacenar los stocks de cada artículo
+
+function agregarAlCarrito(nombre, stock, moneda, costo, elementoBtn) {
+    const cantidadInput = elementoBtn.previousElementSibling;
+    let cantidad = parseInt(cantidadInput.value);
+    let nombreLadrillo = nombre;
+
+    if (!stocks[nombreLadrillo]) {
+        stocks[nombreLadrillo] = stock; // Inicializa el stock para este artículo
+    } 
+
+    if (cantidad <= 0) {
+        alert("Ingresa una cantidad válida");
+        return; // Sale de la función si la cantidad es 0 o negativa
+    }
+
+    if (cantidad > stocks[nombreLadrillo]) {
+        alert("No hay suficientes productos disponibles");
+        return; // Sale de la función si no hay suficiente stock
+    }
+
+    stocks[nombreLadrillo] -= cantidad; // Actualiza el stock en el objeto
+
+    // Actualiza el stock en la interfaz
+    const stockElement = elementoBtn.parentElement.querySelector(".stock");
+    stockElement.textContent = `${stocks[nombreLadrillo]} artículos`;
+
+    console.log(nombreLadrillo);
+    console.log(moneda);
+    console.log(costo);
+    console.log(cantidad);
+    limpiar();
+}
+
+
+
+
